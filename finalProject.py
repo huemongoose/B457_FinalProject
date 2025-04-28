df8e1add7d58e519d77bd1787a4a1edc2af98f70
huemongoose <143539501+huemongoose@users.noreply.github.com>
2025-04-28 19:05:38 -0400

    Add files via upload

--- finalProject.py
+++ finalProject.py
@@ -6,9 +6,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 import numpy as np 
-from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-from sklearn.ensemble import BaggingClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
+
 
 
 
@@ -36,6 +39,7 @@ def classifying(images, labels):
         img = cv2.resize(img, (64,64))
 
         objects = root.findall('object')
+        l=0
         for obj in objects:
             name = obj.find('name').text.lower()
             if name == 'stop':
@@ -49,13 +53,65 @@ def classifying(images, labels):
             labelsLst.append(l)
             break
 
-
-        img = cv2.equalizeHist(img)
         hog = cv2.HOGDescriptor(_winSize=(64, 64),
                         _blockSize=(12, 12),
                         _blockStride=(4, 4),
                         _cellSize=(4, 4),
                         _nbins=9)
+        if l !=3:
+
+            transformed = cv2.flip(img, 1)
+            rows, cols = transformed.shape[:2]
+            M = cv2.getRotationMatrix2D((cols/2, rows/2), 10, 1)
+            rotated = cv2.warpAffine(img, M, (cols, rows))
+            M = np.float32([[1, 0, 5], [0, 1, 0]])
+            shifted = cv2.warpAffine(rotated, M, (cols, rows))
+            shifted = cv2.equalizeHist(shifted)
+            feature = hog.compute(shifted)
+            hogFeatures.append(feature)
+            labelsLst.append(l)
+
+
+            rows, cols = img.shape[:2]
+            M = cv2.getRotationMatrix2D((cols/2, rows/2), 10, 1)
+            rotated = cv2.warpAffine(img, M, (cols, rows))
+            M = np.float32([[1, 0, 5], [0, 1, 0]])
+            shifted = cv2.warpAffine(rotated, M, (cols, rows))
+            shifted = cv2.equalizeHist(shifted)
+            feature = hog.compute(shifted)
+            hogFeatures.append(feature)
+            labelsLst.append(l)
+
+        if l == 1:
+            transformed = cv2.flip(img, 1)
+            rows, cols = transformed.shape[:2]
+            M = cv2.getRotationMatrix2D((cols/2, rows/2), -10, 1)
+            rotated = cv2.warpAffine(img, M, (cols, rows))
+            M = np.float32([[1, 0, 5], [0, 1, 0]])
+            shifted = cv2.warpAffine(rotated, M, (cols, rows))
+            shifted = cv2.equalizeHist(shifted)
+            feature = hog.compute(shifted)
+            hogFeatures.append(feature)
+            labelsLst.append(l)
+        if l == 2:
+            transformed = cv2.flip(img, 1)
+            rows, cols = transformed.shape[:2]
+            M = cv2.getRotationMatrix2D((cols/2, rows/2), -20, 1)
+            rotated = cv2.warpAffine(img, M, (cols, rows))
+            M = np.float32([[1, 0, 5], [0, 1, 0]])
+            shifted = cv2.warpAffine(rotated, M, (cols, rows))
+            shifted = cv2.equalizeHist(shifted)
+            feature = hog.compute(shifted)
+            hogFeatures.append(feature)
+            labelsLst.append(l)
+
+
+
+
+
+
+        img = cv2.equalizeHist(img)
+    
         feature = hog.compute(img)
         hogFeatures.append(feature)
     return hogFeatures,labelsLst
@@ -63,12 +119,18 @@ def classifying(images, labels):
 def KNN(x, y):
     scaler = StandardScaler()
 
-    xTrain, xTest, yTrain, yTest = train_test_split(x, y, random_state=42)
+    xTrain, xTest, yTrain, yTest = train_test_split(x, y,test_size=0.25,random_state=42)
 
     X_scaled = scaler.fit_transform(xTrain)
+    xTest_scaled = scaler.transform(xTest)
 
-    clf = SVC(kernel='linear', gamma=2)
+    weights = {0:10,1:10,2:10,3:1}
+    clf = SVC(kernel='linear',class_weight=weights,C=0.1, gamma=.001)
     clf.fit(X_scaled,yTrain)
+    prediction = clf.predict(xTest_scaled)
+    print(confusion_matrix(yTest,prediction))
+    print(classification_report(yTest,prediction))
+
     return clf, scaler
 
 
@@ -85,6 +147,7 @@ def predictSVM(img, model,scaler):
     feature = hog.compute(img)
     feature = feature.reshape(1, -1)  
     feature_scaled = scaler.transform(feature)
+    
     prediction = model.predict(feature_scaled)
     print(f"Prediction on image: {prediction}")
 
@@ -112,14 +175,15 @@ file5 = "images/road1.png"
 file7 ="images/road847.png"
 file8 = "images/road315.png"
 model,scaler =KNN(x,y)
-predictSVM(file1,model, scaler)
-predictSVM(file2,model, scaler)
+predictSVM(file1,model, scaler)#0
+predictSVM(file2,model, scaler)#3
 
-predictSVM(file3,model, scaler)
-predictSVM(file4,model, scaler)
-predictSVM(file5,model, scaler)
-predictSVM(file7,model, scaler)
-predictSVM(file8,model, scaler)
+predictSVM(file3,model, scaler)#3
+predictSVM(file4,model, scaler)#1
+predictSVM(file5,model, scaler)#1
+predictSVM(file7,model, scaler)#3
+predictSVM(file8,model, scaler)#2
+
 
 
 
